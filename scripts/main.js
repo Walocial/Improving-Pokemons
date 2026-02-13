96e4a96733f0bd6e705847e7c897fb0ebeacbb6d
Walocial <Walocial@hotmail.com>
2026-02-13 19:56:17 +0100

    Grid og List view
    - Simple knapper til at skifte mellem grid og list view med CSS classes
    - Tilføjet hover effekter

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,20 @@ const GENERATION_DROPDOWN = document.getElementById('generations');
 
 GENERATION_DROPDOWN.addEventListener('change', ({target}) => {
     target.value === 'all' ? renderPokemons(allPokemons) : fetchGeneration(target.value);
-})
+});
+
+const LIST_BTN = document.getElementById('listBtn');
+const GRID_BTN = document.getElementById('gridBtn');
+
+LIST_BTN.addEventListener('click', () => {
+    POKEMON_LIST.classList.remove('gridView');
+    POKEMON_LIST.classList.add('listView');
+    GRID_BTN.classList.remove('btn-selected');
+    LIST_BTN.classList.add('btn-selected');
+});
+GRID_BTN.addEventListener('click', () => {
+    POKEMON_LIST.classList.remove('listView');
+    POKEMON_LIST.classList.add('gridView');
+    LIST_BTN.classList.remove('btn-selected');
+    GRID_BTN.classList.add('btn-selected');
+});
