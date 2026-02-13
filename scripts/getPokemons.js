ad46d5b64ace1f53bb384605e5dd7d4804284275
Walocial <Walocial@hotmail.com>
2026-02-13 17:00:44 +0100

    Visning af Pokemons
    - Funktion der render pokemons ud fra et array, så jeg kan hente alle pokemons én enkelt gang, gemme dem i et array, og altid re-render det gemte array, hvis f.eks. brugeren har skiftet til en anden generation, og vil skifte tilbage til at se alle pokemons, så kan jeg blot re-render det gemte array frem for at lave et nyt API-kald.
    - Styling af de viste Pokemons, og klargøring til at lade brugeren skifte mellem et list-view og et grid-view, da det er en meget lang og uoverskuelig liste.

--- scripts/getPokemons.js
+++ scripts/getPokemons.js
@@ -1,12 +1,31 @@
 const POKEMON_LIST = document.getElementById('pokemonList');
+let allPokemons = []; // Array til at gemme Pokemons uden for API kaldets scope
 
+
+
+
+// Funktion til at render pokemons i DOM
+// Tager et array, og render en liste ud fra items
+function renderPokemons(array) {
+    let pokemonList = array;
+
+    pokemonList.forEach(name => {
+        const li = document.createElement('li');
+        li.classList.add('pokemon');
+        li.textContent = name;
+        POKEMON_LIST.appendChild(li);
+    });
+}
 
 async function fetchPokemons() {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
         const data = await response.json();
 
-        console.log(data)
+        allPokemons = data.results.map(pokemon => pokemon.name);
+
+        renderPokemons(allPokemons);
+        console.info('Viser alle Pokemons')
     } catch (error) {
         console.error('Error fetching Pokemons', error);
     }
